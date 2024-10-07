cdac0e17afed3da35ca65c0e39cec8c5506c59cc
gitonboarding <on-boarding@esytes.com>
2024-10-07 17:50:16 +0530

    frontend,index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{HomeController,ProfileController};
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
